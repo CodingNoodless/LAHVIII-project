44b91db1bf6952ff1f341b2c5b5afe8e8abcdff3
SheiakahEye123 <86978045+SheiakahEye123@users.noreply.github.com>
2024-04-06 18:54:14 -0700

    initial?

---
+++ imageproc/archive/torchtest.py
@@ -0,0 +1,47 @@
+import numpy as np
+import torch
+import cv2
+import tarfile
+import os
+import glob
+from PIL import Image
+
+from imageproc.archive.ConvolutionalHandler import CNN
+
+
+def cv2disp(name, image, xpos, ypos):
+    cv2.imshow(name, image)
+    cv2.moveWindow(name, xpos, ypos)
+
+def getBlurry(index):
+    folder_dir = "C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/lowres"
+    image = os.listdir(folder_dir)[index]
+    pil_image = Image\
+        .open("C:/Users/kaide/OneDrive/Desktop/vulcan type ""shit/vulcanfinal/betterproj/imageproc/archive/lowres/" + image)\
+        .resize((250,250))\
+        .convert('HSV')
+    open_cv_image = np.array(pil_image)
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+    return open_cv_image
+def getClear(index):
+    folder_dir = "C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/clear"
+    image = os.listdir(folder_dir)[index]
+    pil_image = Image.open("C:/Users/kaide/OneDrive/Desktop/vulcan type "
+                           "shit/vulcanfinal/betterproj/imageproc/archive/clear/" + image).convert('HSV')
+    open_cv_image = np.array(pil_image)
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+    return open_cv_image
+
+
+blur = getBlurry(0)
+clear = getClear(0)
+
+cnn = CNN(3)
+
+cv2disp("blur", blur, 0,0)
+cv2disp("clear", clear, 100,100)
+
+cnn.forward(blur).
+
+cv2.waitKey(0)
+
