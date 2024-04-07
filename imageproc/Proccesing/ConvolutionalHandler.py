6601d3650a07954cbc3a2caea99176c0fb1db19c
SheiakahEye123 <86978045+SheiakahEye123@users.noreply.github.com>
2024-04-07 00:29:43 -0700

    loss done!

--- imageproc/Proccesing/ConvolutionalHandler.py
+++ imageproc/Proccesing/ConvolutionalHandler.py
@@ -2,11 +2,13 @@ import math
 
 import numpy as np
 from torch import nn
+import torch
 import cv2
 import tarfile
 import os
 import glob
 from PIL import Image
+import torch.nn.functional as F
 
 
 # lets first explain some logic:
@@ -19,23 +21,23 @@ from PIL import Image
 
 
 class ResSequential(nn.Module):
-    def __init__(self, layers, res_scale):
-        super(ResSequential, self).__init__()
+    def __init__(self, layers, res_scale=1.0):
+        super().__init__()
         self.res_scale = res_scale
         self.layers = nn.Sequential(*layers)
 
-    def forward(self, x): return x + self.layers * self.res_scale
+    def forward(self, x): return x + self.layers(x) * self.res_scale
 
 
-def conv(features, inputs, kernel_size=3, atEndofBlock=False):
+def conv(features, inputs, kernel_size=3, atcn=True):
     layers = [nn.Conv2d(inputs, features, kernel_size, padding=kernel_size // 2)]
-    if atEndofBlock:
+    if atcn:
         layers.append(nn.ReLU(True))
-    nn.Sequential(*layers)
+    return nn.Sequential(*layers)
     # resnet blocks are conv->relu->conv, this func sets it up easily
 
 
-def res_block(ni): return [conv(ni, ni), conv(ni, ni, atEndofBlock=True)]
+def res_block(ni): return ResSequential([conv(ni, ni), conv(ni, ni, False)], 0.1)
 
 
 # basic resnet block
@@ -51,17 +53,30 @@ def upsample(inputs, features, scale=10):
 # upsampled block
 
 class deCNN(nn.Module):
-    def __init__(self, scale, features=64, hidden_layers=8):
-        super(deCNN, self).__init__()
+    def __init__(self, scale, features=3, hidden_layers=8):
+        super().__init__()
         layers = []
         # initial conv layer, 3 channels for H, S, V
+        self.scale = scale
 
         layers.append(conv(3, features))
-        for i in range(hidden_layers):
-            layers.append(conv(features, features))
-            layers.append(upsample(features, features, scale))
-            layers.append(nn.BatchNorm2d(features))
-            layers.append(conv(features, 3, atEndofBlock=True))
+        for i in range(hidden_layers): layers.append(res_block(features))
+        layers.append(conv(features, features))
+        layers.append(upsample(features, features, scale))
+        layers.append(nn.BatchNorm2d(features))
+        layers.append(conv(features, 3, False))
         self.layers = nn.Sequential(*layers)
 
-    def forward(self, input): return self.layers(input)
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=self.scale, mode='bilinear', align_corners=False)
+        return x
+
+class SuperResolutionLoss(nn.Module):
+    def __init__(self):
+        super(SuperResolutionLoss, self).__init__()
+
+    def forward(self, output, target):
+        # output = output.view(-1)
+        # target = target.view(-1)
+        loss = F.mse_loss(output, target)
+        return loss
--- imageproc/Proccesing/torchtest.py
+++ imageproc/Proccesing/torchtest.py
@@ -2,39 +2,76 @@ import numpy as np
 import cv2
 import os
 from PIL import Image
+import torch
+import matplotlib.pyplot as plt
 
+from imageproc.Proccesing.ConvolutionalHandler import deCNN, SuperResolutionLoss
+
+finalsize = 250,250
+model = deCNN(10)
+criterion = SuperResolutionLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 
 def cv2disp(name, image, xpos, ypos):
     cv2.imshow(name, image)
     cv2.moveWindow(name, xpos, ypos)
 
+
 def getBlurry(index):
     folder_dir = "C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/lowres/"
     image = os.listdir(folder_dir)[index]
-    pil_image = Image\
-        .open("C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/lowres/" + image)\
-        .resize((250,250))\
-        .convert('HSV')
-    open_cv_image = np.array(pil_image)
-    open_cv_image = open_cv_image[:, :, ::-1].copy()
-    return open_cv_image
+    return "C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/lowres/" + image
+
+
 def getClear(index):
     folder_dir = "C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/clear/"
     image = os.listdir(folder_dir)[index]
-    pil_image = Image.open("C:/Users/kaide/OneDrive/Desktop/vulcan type "
-                           "shit/vulcanfinal/betterproj/imageproc/archive/clear/" + image).convert('HSV')
+    return "C:/Users/kaide/OneDrive/Desktop/vulcan type shit/vulcanfinal/betterproj/imageproc/archive/clear/" + image
+
+
+def load_and_preprocess_image(image_path, size):
+    image = Image.open(image_path)
+    image = image.resize(size)
+    image_tensor = torch.tensor(np.array(image), dtype=torch.float32).permute(2, 0, 1) / 255.0
+    return image_tensor.unsqueeze(0)
+
+
+def draw(filepath1, filepath2, output):
+    pil_image = Image.open(filepath1).resize((finalsize), 4)
     open_cv_image = np.array(pil_image)
     open_cv_image = open_cv_image[:, :, ::-1].copy()
-    return open_cv_image
+    cv2disp("blur", np.array(open_cv_image), 0, 0)
 
+    pil_image = Image.open(filepath2).resize((finalsize), 4)
+    open_cv_image = np.array(pil_image)
+    open_cv_image = open_cv_image[:, :, ::-1].copy()
+    cv2disp("clear", np.array(open_cv_image), 250, 0)
 
-blur = getBlurry(500)
-clear = getClear(500)
+    open_cv_image = np.array(output)
+    open_cv_image = np.squeeze(open_cv_image, axis=0)
+    open_cv_image = open_cv_image.T
+    open_cv_image = np.flip(open_cv_image)
+    open_cv_image = np.rot90(open_cv_image)
+    open_cv_image = np.flip(open_cv_image, axis=1)
+    cv2disp("model", open_cv_image, 500, 0)
 
+def runOne(epochs):
+    losses = []  # List to store the loss for each epoch
 
-cv2disp("blur", blur, 0,0)
-cv2disp("clear", clear, 100,100)
+    learner = SuperResolutionLoss()
+    for epoch in range(epochs):
+        blur = load_and_preprocess_image(getBlurry(epoch), (25, 25))
+        clear = load_and_preprocess_image(getClear(epoch), finalsize)
 
-cv2.waitKey(0)
+        output = model.forward(blur)
+        loss = learner.forward(output, clear)
+        print("Loss:", loss.item())
+        draw(getBlurry(epoch), getClear(epoch), output)
+        optimizer.zero_grad()
+        losses.append(loss.item())
+        loss.backward()
+        optimizer.step()
+        cv2.waitKey(1)
 
+runOne(100)
